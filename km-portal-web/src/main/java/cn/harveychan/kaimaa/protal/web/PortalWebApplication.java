7483772d9665b6bf2ce65eab7485be146d05ba7a
Harvey Chan <harveychan@163.com>
2020-03-09 10:36:00 +0800

    新增网站前端相关模块

---
+++ km-portal-web/src/main/java/cn/harveychan/kaimaa/protal/web/PortalWebApplication.java
@@ -0,0 +1,12 @@
+package cn.harveychan.kaimaa.protal.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalWebApplication.class, args);
+    }
+}
